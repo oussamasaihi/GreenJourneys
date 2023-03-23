4a0eef86a1c6d0383effaa650d32ad6f1c56fb62
swilhi <ameni.swilhi@esprit.tn>
2023-03-23 23:19:24 +0100

    ameni

--- src/main/java/com/greenjourneys/entities/Event.java
+++ src/main/java/com/greenjourneys/entities/Event.java
@@ -1,4 +1,29 @@
 package com.greenjourneys.entities;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@ToString
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long idEvent;
+    private LocalDate dateDebutEvent;
+    private LocalDate dateFinEvent;
+    private String regionEvent;
+    private String nomEvent;
+    private String descriptionEvent;
+
+    @ManyToOne
+    private User user;
+
 }
--- src/main/java/com/greenjourneys/entities/Transport.java
+++ src/main/java/com/greenjourneys/entities/Transport.java
@@ -1,4 +1,26 @@
 package com.greenjourneys.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@ToString
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+
 public class Transport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+
+    private Integer idTransport;
+    @Enumerated(EnumType.STRING)
+    private Type_Moyen type_moyen;
+    private Long distance;
+
+    @ManyToOne
+    private User user;
+
 }
