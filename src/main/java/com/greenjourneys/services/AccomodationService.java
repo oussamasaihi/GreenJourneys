c6c6e46eee4789231b16db35dd9cc9ac0faafbf7
oussama <oussama.saihi@esprit.tn>
2023-03-30 00:47:21 +0100

    project

--- src/main/java/com/greenjourneys/services/AccomodationService.java
+++ src/main/java/com/greenjourneys/services/AccomodationService.java
@@ -1,4 +1,32 @@
 package com.greenjourneys.services;
 
-public class AccomodationService {
+import com.greenjourneys.entities.Accomodation;
+
+import java.util.List;
+
+public class AccomodationService implements IAccomodationService{
+    @java.lang.Override
+    public Accomodation add(Accomodation entity) {
+        return null;
+    }
+
+    @java.lang.Override
+    public Accomodation update(Accomodation entity) {
+        return null;
+    }
+
+    @java.lang.Override
+    public Accomodation retrieveById(Long aLong) {
+        return null;
+    }
+
+    @java.lang.Override
+    public List<Accomodation> retrieveAll() {
+        return null;
+    }
+
+    @java.lang.Override
+    public Boolean delete(Long aLong) {
+        return null;
+    }
 }
